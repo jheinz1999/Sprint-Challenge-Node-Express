157d0c66acd534c6c6240a8d3e763cec3998cf49
jheinz1999 <jonathan.heinz@aol.com>
2019-01-11 08:26:53 -0800

    set up server structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+
+  res.status(200).send('Working!');
+
+});
+
+module.exports = server;
