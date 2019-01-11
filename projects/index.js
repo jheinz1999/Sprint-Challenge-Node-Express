a40251db8ff24dfab854669751e2fe5dd6df3a80
jheinz1999 <jonathan.heinz@aol.com>
2019-01-11 08:37:30 -0800

    GET to /api/products/:id returns correct data

--- projects/index.js
+++ projects/index.js
@@ -22,4 +22,23 @@ server.get('/', async (req, res) => {
 
 });
 
+server.get('/:id', async (req, res) => {
+
+  const id = req.params.id;
+
+  try {
+
+    const project = await projectDB.get(id);
+    res.status(200).json(project);
+
+  }
+
+  catch (err) {
+
+    res.status(404).json({message: 'The project with that ID does not exist.'});
+
+  }
+
+});
+
 module.exports = server;
